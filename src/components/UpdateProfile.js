70d6cdb3f51332e7076ca007036c2dd848a606b5
mokumus <mokumus1996@gmail.com>
2021-01-11 20:20:54 +0300

    Email/Password Login and Dashboard added

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -4,32 +4,42 @@ import { Form, Button, Card, Alert} from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 import { Link, useHistory } from 'react-router-dom'
 
-export default function Signup(){
+export default function UpdateProfile(){
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
-    const { signup } = useAuth()
+    const { currentUser, updatePassword, updateEmail } = useAuth()
     const [ error, setError] = useState('')
     const [ loading, setLoading] = useState(false)
     const history = useHistory()
 
-    async function handleSubmit(e) {
+ function handleSubmit(e) {
+        
         e.preventDefault()
 
         if(passwordRef.current.value !== passwordConfirmRef.current.value) {
             return setError('Passwords do not match')
         }
 
-        try {
-            setError('')
-            setLoading(true)
-            await signup(emailRef.current.value, passwordRef.current.value)
-            history.push("/")
-        } catch {
-            setError('Failed to create an account')
-        }
-        setLoading(false)
+        const promises = []
+        setLoading(true)
+        setError("")
 
+        if(emailRef.current.email !== currentUser.email){
+            promises.push(updateEmail(emailRef.current.value))
+        }
+
+        if(passwordRef.current.value) {
+            promises.push(updatePassword(passwordRef.current.value))
+        }
+
+        Promise.all(promises).then(() => {
+            history.push("/")
+        }).catch(() => {
+            setError("Failed to update account")
+        }).finally(() => {
+            setLoading(false)
+        })
         
     }
 
@@ -37,27 +47,27 @@ export default function Signup(){
         <div>
             <Card>
                 <Card.Body>
-                    <h2 className="text-center mb-4">Sign Up</h2>
+                    <h2 className="text-center mb-4">Update Profile</h2>
                     {error && <Alert variant="danger">{error}</Alert>}
                     <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" ref={emailRef} required></Form.Control>
+                            <Form.Control type="email" ref={emailRef} defaultValue={currentUser.email}></Form.Control>
                         </Form.Group>
                         <Form.Group id="password">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" ref={passwordRef} required></Form.Control>
+                            <Form.Control type="password" ref={passwordRef} placeholder="Leave blank to keep the same"></Form.Control>
                         </Form.Group>
                         <Form.Group id="password-confirm">
                             <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmRef} required></Form.Control>
+                            <Form.Control type="password" ref={passwordConfirmRef} placeholder="Leave blank to keep the same"></Form.Control>
                         </Form.Group>
-                        <Button disabled={loading} class="w-100" type="submit">Sign Up</Button>
+                        <Button disabled={loading} class="w-100" type="submit">Update</Button>
                     </Form>
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
-                Already have an account?<Link to="/login"> Login</Link>
+                <Link to="/"> Cancel</Link>
             </div>
         </div>
     )

2ee444ff76028f6d45c2d0ae4d5e6d033c93997f
mokumus <mokumus1996@gmail.com>
2021-01-11 20:28:55 +0300

    Fixed  routing for github pages v2

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import React from 'react'
 import Signup from './Signup'
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from '../contexts/AuthContext'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { HashRouter as Router, Switch, Route} from 'react-router-dom'
 import Dashboard from "./Dashboard"
 import Login from "./Login"
 import PrivateRoute from "./PrivateRoute"
@@ -16,7 +16,7 @@ function App(){
       
         <Container className="d-flex align-items-center justify-content-center" style={{minHeight: "100vh"}}>
           <div className="w-100" style={{maxWidth:'400px'}}>
-            <Router basename={process.env.PUBLIC_URL}>
+            <Router>
               <AuthProvider>
                 <Switch>
                   <PrivateRoute exact path="/" component={Dashboard} />
